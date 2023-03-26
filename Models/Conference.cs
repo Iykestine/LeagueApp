78ee605ec05812cf8556a2ad7331c7681c55904b
Iykestine <roboticike@yahoo.com>
2023-03-26 16:59:32 +0100

    Added models and seeded database

---
+++ Models/Conference.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeagueApp.Models
+{
+    public class Conference
+    {
+        public string LeagueId { get; set; }
+        public string ConferenceId { get; set; }
+        public string Name { get; set; }
+
+        public League League { get; set; }
+    }
+}
---
+++ Models/League.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeagueApp.Models
+{
+    public class League
+    {
+        public string LeagueId { get; set; }
+        public string Name { get; set; }
+    }
+}
