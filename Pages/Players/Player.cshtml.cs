ede5c8dc547caa6b4a3e2b4c60a8aad427989f0b
Iykestine <roboticike@yahoo.com>
2023-08-29 16:23:46 +0100

    Added Player Details page

--- Pages/Players/Player.cshtml.cs
+++ Pages/Players/Player.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LeagueApp.Data;
+using LeagueApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,22 @@ namespace LeagueApp.Pages.Players
 {
     public class PlayerModel : PageModel
     {
-        public void OnGet()
+        // inject the Entity Framework context
+
+        private readonly LeagueContext _context;
+
+        public PlayerModel(LeagueContext context)
         {
+            _context = context;
+        }
+
+        // load a single player based on Id
+
+        public Player Player { get; set; }
+
+        public async Task OnGetAsync(string Id)
+        {
+            Player = await _context.Players.FindAsync(Id);
         }
     }
 }
